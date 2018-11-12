35b6cdb47c6a5493138396c3f95704f7c870f019
Robin Tram <robin.tram@gmail.com>
2018-11-12 17:34:29 -0500

    Added Planet Current Donations Method, and Basic Template for Planets

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -3,4 +3,10 @@ class Planet < ApplicationRecord
   has_many :colonists, through: :trips
   has_many :donations
   has_many :sponsors, through: :donations
+
+  def funding_gap
+    donation_amounts_arr = self.donations.collect {|donation_obj| donation_obj.amount}
+    current_donations_int = donation_amounts_arr.sum
+    self.funding_goal - current_donations_int
+  end
 end
