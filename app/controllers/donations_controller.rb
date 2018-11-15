ee8156aae8c7c27b073ce1caa1acb26c7dba5bf4
Robin Tram <robin.tram@gmail.com>
2018-11-15 10:32:10 -0500

    Donation Bug Fixed

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -8,7 +8,7 @@ class DonationsController < ApplicationController
     @donation = Donation.new
     @donation.planet_id = donation_params[:planet_id]
     @donation.amount = donation_params[:amount]
-    @donation.sponsor_id = session[:sponsor_id]
+    @donation.sponsor_id = session[:id]
     if @donation.sponsor_id.nil? #BECAUSE NOT LOGGED IN
       flash.notice = "Please Login as a Sponsor to Donate"
       redirect_to login_path
