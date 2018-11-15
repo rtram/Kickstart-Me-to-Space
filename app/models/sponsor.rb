62d4af550db63fafb09d7c3f834fda2f4d0f17b4
Robin Tram <robin.tram@gmail.com>
2018-11-15 10:20:38 -0500

    Net Worth, Donation Rankings

--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -14,4 +14,44 @@ class Sponsor < ApplicationRecord
   def full_name
     "#{self.first_name} #{self.last_name}"
   end
+
+#Top Ten Generous Stat----------------------------------------------------------
+  def donation_objects
+    self.donations.collect {|donation| donation[:amount]}
+  end
+
+  def donation_sum
+    self.donation_objects.sum
+  end
+
+  def self.donations_sum_arr
+    self.all.collect {|sponsor| {sponsor.full_name => sponsor.donation_sum}}
+  end
+
+  def self.sorted_donations_sum_arr
+    self.donations_sum_arr.sort! {|a, b| a.values[0] <=> b.values[0]}
+  end
+
+  def self.top_ten_overall
+    self.sorted_donations_sum_arr.reverse![0..9]
+  end
+
+#Top Ten Most Stingy Stat-------------------------------------------------------
+  def self.bottom_ten_overall
+    self.sorted_donations_sum_arr[0..9]
+  end
+
+#Net Worth Stats----------------------------------------------------------
+  def self.net_worth_arr
+    self.all.sort_by {|sponsor| sponsor[:net_worth]}
+  end
+
+  def self.richest_sponsor
+    self.net_worth_arr.last
+  end
+
+  def self.poorest_sponsor
+    self.net_worth_arr.first
+  end
+
 end
